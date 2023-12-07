97b33a7b96c34c85e51088c107b3fc92165d3412
Linus <97843108+lynx197@users.noreply.github.com>
2023-12-07 21:01:36 +0100

    Added mobile navbar menu (WIP)

--- components/header-component.js
+++ components/header-component.js
@@ -23,7 +23,6 @@ class Header extends HTMLElement {
             padding: 1.5px;
             filter: drop-shadow(var(--drop-shadow));
             background-color: var(--bg-header);
-            position: relative;
         }
 
         .navbar-a {
@@ -35,7 +34,7 @@ class Header extends HTMLElement {
 
         .navbar-big {
             flex-grow: 5;
-            font-size:xx-large;
+            font-size: xx-large;
         }
 
         .navbar-brand {
@@ -49,14 +48,48 @@ class Header extends HTMLElement {
             }
         }
 
-        @media screen and (max-width: 700px) {
-            .navbar-a {
-                display: none;
-            }
+        .icon-bar {
+          width: 35px;
+          height: 5px;
+          background-color: var(--title);
+          margin: 6px 0;
         }
 
+        /* Hide the link that should open and close the topnav on small screens */
+        .navbar .icon {
+          display: none;
+        }
+
+        /* When the screen is less than 600 pixels wide, hide all links, except for the first one ("Home"). Show the link that contains should open and close the topnav (.icon) */
+       @media screen and (max-width: 600px) {
+         .navbar a:not(:nth-child(0n+3)) {display: none;}
+         .navbar a.icon {
+           float: right;
+           display: block;
+         }
+       }
+
+       /* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
+       @media screen and (max-width: 600px) {
+         .navbar.responsive {
+           position: relative;
+           flex-direction: column;
+         }
+         .navbar.responsive a.icon {
+           position: fixed;
+           right: 0;
+           top: 0;
+         }
+         .navbar.responsive a {
+           display: relative;
+           float: none;
+           display: block;
+           text-align: left;
+         }
+       }
+
       </style>
-      <div class="navbar">
+      <div class="navbar" id="navbar">
         <a class="navbar-a" href="construction.html">
             <div class="navbar-brand">Teams</div>
         </a>
@@ -72,9 +105,24 @@ class Header extends HTMLElement {
         <a class="navbar-a" href="sources.html">
             <div class="navbar-brand">Quellen</div>
         </a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <div class="icon-bar"></div>
+          <div class="icon-bar"></div>
+          <div class="icon-bar"></div>
+        </a>
     </div>
       `;
     }
 }
 
 customElements.define("header-component", Header);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
